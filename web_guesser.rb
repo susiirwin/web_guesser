139fba1a86cba5e3cca2545bc32752872407483a
SusiIrwin <susi.irwin@gmail.com>
2016-08-10 10:55:24 -0600

    removes an unused variable

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,8 +7,7 @@ get '/' do
   guess = params["guess"]
   message = check_guess(guess)
   erb :index, :locals => {:number => SECRET_NUMBER,
-                          :message => message,
-                          :win_message => win}
+                          :message => message}
 end
 
 def check_guess(guess)
